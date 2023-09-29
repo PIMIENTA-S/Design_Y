663b8c420fa6d3848ee36fa912acb115fd41e65a
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-09-29 16:39:00 -0500

    Add keydown

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const date = document.getElementById('date');
+const promtInput = document.getElementById('promtInput')
+const terminal = document.getElementById('terminal');
+const terminalWindow = document.getElementById('terminalWindow');
 
-date.innerText = new Date().toDateString()
+// una vez inicia el programa el usuario sobre escribir 
+promtInput.focus();
+// agrega la fecha en la parte superior de la terminal
+date.innerText = new Date().toDateString();
+// una vez el usuaria da click sobre el recuadro de la terminal este pueda escribir de inmediato
+terminalWindow.addEventListener('click', () => promtInput.focus());
+
+promtInput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        enterCommand(event)
+    }
+})
+
+// crear funcion enterCommand
